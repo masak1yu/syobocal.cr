3aa704fc141a9e312fbca29fcecdacceb552e1c0
msky026 <pref.myu@gmail.com>
2018-07-18 20:47:09 +0900

    add db,json a few api

--- src/syobocal.cr
+++ src/syobocal.cr
@@ -1,6 +1,82 @@
 require "./syobocal/*"
+require "http/client"
+require "json"
+require "xml"
 
-# TODO: Write documentation for `Syobocal`
+command = ARGV[0]
+params = ARGV[1] if ARGV[1]
+
+case command
+when "DB::TitleLookup"
+  puts Syobocal::DB::TitleLookup.url(params)
+  result = Syobocal::DB::TitleLookup.get(params)
+  p result
+when "JSON::TitleMedium"
+  puts Syobocal::JSON::TitleMedium.url(params)
+  result = Syobocal::JSON::TitleMedium.get(params)
+  p result
+when "JSON::TitleFull"
+  puts Syobocal::JSON::TitleMedium.url(params)
+  result = Syobocal::JSON::TitleMedium.get(params)
+  p result
+when "JSON::ProgramByPID"
+  puts Syobocal::JSON::ProgramByPID.url(params)
+  result = Syobocal::JSON::ProgramByPID.get(params)
+  p result
+end
+
 module Syobocal
-  # TODO: Put your code here
+  module JSON
+    class TitleMedium
+      def self.get(params)
+        response = HTTP::Client.get(url(params))
+        result = ::JSON.parse(response.body)
+      end
+
+      def self.url(params)
+        "http://cal.syoboi.jp/json.php?Req=TitleMedium" + "&" + params.to_s
+      end
+    end
+
+    class TitleFull
+      def self.get(params)
+        response = HTTP::Client.get(url(params))
+        result = ::JSON.parse(response.body)
+      end
+
+      def self.url(params)
+        "http://cal.syoboi.jp/json.php?Req=TitleFull" + "&" + params.to_s
+      end
+    end
+
+    class ProgramByPID
+      def self.get(params)
+        response = HTTP::Client.get(url(params))
+        result = ::JSON.parse(response.body)
+      end
+
+      def self.url(params)
+        "http://cal.syoboi.jp/json.php?Req=ProgramByPID" + "&" + params.to_s
+      end
+    end
+  end
+end
+
+module Syobocal
+  module DB
+    class TitleLookup
+      def self.get(params)
+        response = HTTP::Client.get(url(params))
+        parse(response.body)
+      end
+
+      def self.url(params)
+        "http://cal.syoboi.jp/db.php?Command=TitleLookup" + "&" + params.to_s
+      end
+
+      def self.parse(xml)
+        ::XML.parse(xml)
+      end
+    end
+  end
 end
